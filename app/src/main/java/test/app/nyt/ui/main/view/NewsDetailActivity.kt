59883bc92d51717483c52cd53e126bfcd3492a0b
iMac Web <midhunennakkandi@gmail.com>
2020-07-12 18:59:13 +0400

    Added Image

--- app/src/main/java/test/app/nyt/ui/main/view/NewsDetailActivity.kt
+++ app/src/main/java/test/app/nyt/ui/main/view/NewsDetailActivity.kt
@@ -2,8 +2,12 @@ package test.app.nyt.ui.main.view
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import dagger.hilt.android.AndroidEntryPoint
 import test.app.nyt.R
 import test.app.nyt.data.model.nytdata.Result
@@ -12,7 +16,23 @@ import test.app.nyt.databinding.ActivityDetailBinding
 
 @AndroidEntryPoint
 class NewsDetailActivity : AppCompatActivity() {
+    companion object {
 
+        @JvmStatic
+        @BindingAdapter("banner")
+        fun loadImage(
+            imageView: ImageView,
+            imageURL: String?
+        ) {
+            Glide.with(imageView.context)
+                .setDefaultRequestOptions(
+                    RequestOptions()
+                        .override(300, 500)
+                )
+                .load(imageURL)
+                .into(imageView)
+        }
+    }
     var binding: ActivityDetailBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +64,18 @@ class NewsDetailActivity : AppCompatActivity() {
     }
     private fun parseData(result: Result) {
         binding?.news = result
+
+        if (result?.media.isNotEmpty()){
+            if (result.media[0].mediametadata.isNotEmpty()){
+
+                var mediaList = result.media[0]?.mediametadata
+                if (mediaList?.size>2){
+                    binding?.imageUrl = mediaList[2]?.url
+                }
+
+
+            }
+        }
 
     }
 
